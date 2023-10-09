a8c786a7fc527fa1eb49e892b48ce9a11d9c5f5c
felixcapuano <felix.capuano@gmail.com>
2023-10-09 14:12:35 +0200

    fix static file

--- csgoscan/main.py
+++ csgoscan/main.py
@@ -8,7 +8,7 @@ from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/static", StaticFiles(directory="csgoscan/static"), name="static")
 templates = Jinja2Templates(directory="csgoscan/templates")
 
 websites = [faceit_finder, cs_stats, csgo_backpack, leetify]
